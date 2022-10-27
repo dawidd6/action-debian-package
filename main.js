14217920f965e1f0c0dd6cff8c97c1a8c0e8dcaa
Enno Rehling <enno.rehling@gmail.com>
2022-10-27 09:00:28 +0200

    replace missing qemu 5.2 with 6.2 (#58)

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ async function main() {
         if (cpuArchitecture != "amd64") {
             core.startGroup("Install QEMU")
             // Need newer QEMU to avoid errors
-            await exec.exec("wget", ["http://mirrors.kernel.org/ubuntu/pool/universe/q/qemu/qemu-user-static_5.2+dfsg-9ubuntu2_amd64.deb", "-O", "/tmp/qemu.deb"])
+            await exec.exec("wget", ["http://mirrors.kernel.org/ubuntu/pool/universe/q/qemu/qemu-user-static_6.2+dfsg-2ubuntu6_amd64.deb", "-O", "/tmp/qemu.deb"])
             await exec.exec("sudo", ["dpkg", "-i", "/tmp/qemu.deb"])
             core.endGroup()
         }
